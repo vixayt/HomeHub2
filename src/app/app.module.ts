0b09c55818a6f9b9af57dc0b020f01cf140debc7
Theodore Vixay <vixay@pdx.edu>
2019-10-14 23:03:51 -0700

    added template for web pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TrimetComponent } from './trimet/trimet.component';
+import { RouterModule, Routes } from '@angular/router';
+import { WeatherComponent } from './weather/weather.component';
+
+const appRoutes: Routes = [
+  { path: 'trimet', component: TrimetComponent },
+  { path: 'weather', component: WeatherComponent }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, TrimetComponent, WeatherComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/trimet/trimet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trimet',
+  templateUrl: './trimet.component.html',
+  styleUrls: ['./trimet.component.scss']
+})
+export class TrimetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
