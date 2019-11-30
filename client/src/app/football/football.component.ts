7a3d11b6504b8fa386c866818035f3688e234065
jordan-childs20 <jsd9@pdx.edu>
2019-11-30 11:54:39 -0800

    adding kick and punt return data, modified interafce

--- client/src/app/football/football.component.ts
+++ client/src/app/football/football.component.ts
@@ -32,72 +32,68 @@ export class FootballComponent implements OnInit {
       });
     }
 
-    selectedTeam(team: string)
-    {
-      this.team = team;
-    }
+  selectedTeam(team: string)
+  {
+    this.team = team;
+  }
 
-    selectedYear(year: string)
-    {
-      this.year = year;
-    }
+  selectedYear(year: string)
+  {
+    this.year = year;
+  }
 
-    getTeamsGames() {
-      if(this.team == '' || this.year == undefined){
-        this.year = this.years[0];
-        this.team = this.teams[0];
-        return;
-      } else {
-        let params = new HttpParams();
-        params = params.append("team", this.team);
-        params = params.append("year", this.year);
-        this.http
-          .get<any>('/api/games', {
-            params: params
-          })
-          .subscribe(data =>{
-            this.gameData = [];
-            for(var d in data)
-            { 
-              this.gameData.push(JSON.parse(JSON.stringify(data[d])));
-              this.gameData[d].gameLeaders= {};
-              this.gameData[d].expanded = false;
-            }
-            this.received_data= true;
-            this.getLeaders();
-          });   
-      }
+  getTeamsGames() {
+    if(this.team == '' || this.year == undefined){
+      this.year = this.years[0];
+      this.team = this.teams[0];
+      return;
+    } else {
+      let params = new HttpParams();
+      params = params.append("team", this.team);
+      params = params.append("year", this.year);
+      this.http
+        .get<any>('/api/games', {
+          params: params
+        })
+        .subscribe(data =>{
+          this.gameData = [];
+          for(var d in data)
+          { 
+            this.gameData.push(JSON.parse(JSON.stringify(data[d])));
+            this.gameData[d].gameLeaders= {};
+            this.gameData[d].expanded = false;
+            this.gameData[d].home_team_data = true;
+          }
+          this.received_data= true;
+          this.getLeaders();
+        });   
     }
+  }
 
-    async getLeaders() {
-    
-      for(var g in this.gameData)
-      {
+  async getLeaders() {
   
-        console.log("hello");
-        let params: any;
-        this.gameData[g].gameLeaders.home_team = {}
-        this.gameData[g].gameLeaders.away_team = {}
-        //get leaders for home team
-        params = new HttpParams();
+    for(var g in this.gameData)
+    {
+      let params: any;
+      this.gameData[g].gameLeaders.home_team = {}
+      this.gameData[g].gameLeaders.away_team = {}
+     
+      //get leaders for home team
+      params = new HttpParams();
+      params = params.append("team", this.gameData[g].home_team);
+      params = params.append("game", this.gameData[g].game_id);
+      var home_response = await this.http.get<any>('api/leaders', {params: params}).toPromise();
+      this.gameData[g].gameLeaders.home_team = JSON.parse(JSON.stringify(home_response));
       
-        params = params.append("team", this.gameData[g].home_team);
-        params = params.append("game", this.gameData[g].game_id);
-        console.log(this.gameData[g].home_team);
-        console.log(this.gameData[g].game_id);
-        var home_response = await this.http.get<any>('api/leaders', {params: params}).toPromise();
-        console.log(home_response);
-        this.gameData[g].gameLeaders.home_team = JSON.parse(JSON.stringify(home_response));
-        
-        params = new HttpParams();
-        params = params.append("team", this.gameData[g].away_team);
-        params = params.append("game", this.gameData[g].game_id);
-        var away_response = await this.http.get<any>('api/leaders', {params: params}).toPromise();
-        console.log(away_response);
-        this.gameData[g].gameLeaders.away_team = JSON.parse(JSON.stringify(away_response));
-        
-      }
+      //get away team leaders
+      params = new HttpParams();
+      params = params.append("team", this.gameData[g].away_team);
+      params = params.append("game", this.gameData[g].game_id);
+      var away_response = await this.http.get<any>('api/leaders', {params: params}).toPromise();
+      this.gameData[g].gameLeaders.away_team = JSON.parse(JSON.stringify(away_response));
+
     }
+  }
 }
 
 
